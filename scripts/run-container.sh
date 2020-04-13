d6d31c13096ef7bb12baba58f2526e6a4107cf3f
kaotu <kao-tu@hotmail.co.th>
2020-04-14 01:53:24 +0700

    Update script for container 🌚

--- scripts/run-container.sh
+++ scripts/run-container.sh
@@ -3,9 +3,13 @@
 echo Loggin AWS ECR
     $(aws ecr get-login --region ap-southeast-1 --no-include-email)
 
+echo $IMAGE_REPO_NAME
+echo $IMAGE_TAG
+
 if [ "$DEPLOYMENT_GROUP_NAME" == "test-deploy-docker-group" ]
 then
-   docker pull 131304515891.dkr.ecr.ap-southeast-1.amazonaws.com/reactapp-tesy:latest 
-   docker run -p 3000:3000 -d 131304515891.dkr.ecr.ap-southeast-1.amazonaws.com/reactapp-tesy:latest
+   docker pull 131304515891.dkr.ecr.ap-southeast-1.amazonaws.com/reactapp-test:latest
+   docker run -p 3000:3000 -d 131304515891.dkr.ecr.ap-southeast-1.amazonaws.com/reactapp-test:latest
+
 fi
 
