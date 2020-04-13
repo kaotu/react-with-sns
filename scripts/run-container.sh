4d2343cf52ba858c1de1cdbf61a13c3c0ecf920c
kaotu <kao-tu@hotmail.co.th>
2020-04-14 01:31:16 +0700

    Update pull image to local 🌚

--- scripts/run-container.sh
+++ scripts/run-container.sh
@@ -2,5 +2,9 @@
 
 echo Loggin AWS ECR
     $(aws ecr get-login --region ap-southeast-1 --no-include-email)
+
+echo Pull image
     docker pull $IMAGE_REPO_NAME:$IMAGE_TAG 
-    docker run --name react -p 3000:3000 -d $IMAGE_REPO_NAME:$IMAGE_TAG
+
+echo run app
+    docker run -p 3000:3000 -d $IMAGE_REPO_NAME:$IMAGE_TAG
