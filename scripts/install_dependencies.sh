5575a8c5a804e5feceaea3d61b4c38330a5cf396
kaotu <kao-tu@hotmail.co.th>
2020-04-13 21:55:24 +0700

    Add dependency of node 🌚

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_12.x | bash -
-apt install -y nodejs
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-apt install --no-install-recommends yarn
+sudo curl -sL https://deb.nodesource.com/setup_12.x | bash -
+sudo apt install -y nodejs
+sudo curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+sudo apt install --no-install-recommends yarn
