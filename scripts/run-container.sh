311055e61fb21e8339baf3b130bfadddbc9714d0
kaotu <kao-tu@hotmail.co.th>
2020-04-14 01:24:59 +0700

    Add pull image to local 🌚

--- scripts/run-container.sh
+++ scripts/run-container.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run --name react -p 3000:3000 -d $IMAGE_REPO_NAME:$IMAGE_TAG
+echo Loggin AWS ECR
+    $(aws ecr get-login --region ap-southeast-1 --no-include-email)
+    docker pull $IMAGE_REPO_NAME:$IMAGE_TAG 
+    docker run --name react -p 3000:3000 -d $IMAGE_REPO_NAME:$IMAGE_TAG
